6cb899b7fd4a5be48cc94d54df5313e5f393920b
Priyabrata Mohapatra <31694794+Priyabrata017@users.noreply.github.com>
2018-10-17 10:19:38 +0530

    Used PCA to reduce the dimensions and used logistic regression inplace of gaussian bayes and improved the accuracy from 90% to 94%

--- Day-27 Kaggle Breast Cancer Prediction/Breast_Cancer_prediction.py
+++ Day-27 Kaggle Breast Cancer Prediction/Breast_Cancer_prediction.py
@@ -15,7 +15,12 @@ plt.show()
 diag=pd.get_dummies(data['diagnosis'],drop_first=True)
 data.drop(['diagnosis','id'],axis=1,inplace=True)
 data=pd.concat([data,diag],axis=1)
-
+#PCA to reduce the features
+from sklearn.decomposition import PCA
+pca=PCA(n_components=2)
+X_train=pca.fit_transform(X_train)
+X_test=pca.transform(X_test)
+variance=pca.explained_variance_ratio_
 
 X=data.iloc[: ,0:30]
 y=data.iloc[:, 31:32]
@@ -31,13 +36,21 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 #Using Gaussian Naive Bayes model to train the model
-from sklearn.naive_bayes import GaussianNB
+'''from sklearn.naive_bayes import GaussianNB
 clf=GaussianNB()
-clf.fit(X_train,y_train)
+clf.fit(X_train,y_train)'''
+
+#Using Logistic Regression to predict the model
+
+from sklearn.linear_model import LogisticRegression
+lg=LogisticRegression()
+lg.fit(X_train,y_train)
+y2_pred=lg.predict(X_test)
+
 #Predicting the result
-predict=clf.predict(X_test)
+predict=lg.predict(X_test)
 from sklearn.metrics import accuracy_score,confusion_matrix
-a=accuracy_score(predict,y_test)  #0.9190=91%
-cm=confusion_matrix(y_test,predict)
-'''[[84,  6],
-       [ 6, 47]], dtype=int64)'''
+a=accuracy_score(y2_pred,y_test)  #0.93.7%
+cm=confusion_matrix(y_test,y2_pred)
+'''array([[84,  6],
+       [ 3, 50]], dtype=int64)'''
